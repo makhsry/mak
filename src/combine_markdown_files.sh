8f6b0cd71bbac999c04062f49efdf0a77aa4c28e
MAK <165974478+makhsry@users.noreply.github.com>
2025-10-01 21:56:57 -0700

    Update combine_markdown_files.sh

--- src/combine_markdown_files.sh
+++ src/combine_markdown_files.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-input_dir = "assets/md_files/"
-output_file = "README.md"
+input_dir="assets/md_files/"
+output_file="README.md"
 
-#
+# Clear or create output file
+> "${output_file}"
 
-cat ${input_dir}Intro_Professional_Summary.md >> ${output_file}
-cat "   " >> ${output_file}
-cat ${input_dir}Intro_Organizational_Culture.md >> ${output_file}
-cat "   " >> ${output_file}
-cat ${input_dir}Intro_Technical_Summary.md >> ${output_file}
-cat "   " >> ${output_file}
-cat ${input_dir}Intro_Contact.md >> ${output_file}
-cat "   " >> ${output_file}
+cat "${input_dir}Intro_Professional_Summary.md" >> "${output_file}"
+echo -e "\n\n" >> "${output_file}"
 
-# 
+cat "${input_dir}Intro_Organizational_Culture.md" >> "${output_file}"
+echo -e "\n\n" >> "${output_file}"
+
+cat "${input_dir}Intro_Technical_Summary.md" >> "${output_file}"
+echo -e "\n\n" >> "${output_file}"
+
+cat "${input_dir}Intro_Contact.md" >> "${output_file}"
+echo -e "\n\n" >> "${output_file}"
