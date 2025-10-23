d73f41cb9e750282a6e5aff6711eb44ddd339bbd
MAK <165974478+makhsry@users.noreply.github.com>
2025-10-22 22:47:55 -0700

    Update combine_markdown_files.sh
    disabled header table

--- src/combine_markdown_files.sh
+++ src/combine_markdown_files.sh
@@ -6,9 +6,8 @@ output_file="README.md"
 # Clear or create output file
 > "${output_file}"
 
-cat "${input_dir}headers-top.md" >> "${output_file}"
-echo -e "\n\n" >> "${output_file}"
-
+#cat "${input_dir}headers-top.md" >> "${output_file}"
+#echo -e "\n\n" >> "${output_file}"
 
 cat "${input_dir}introduction.md" >> "${output_file}"
 echo -e "\n\n" >> "${output_file}"
